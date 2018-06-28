c32b80ea370402b42602270210af6f34ab91e04b
Kelly Smith <k.kmsmith.smith@gmail.com>
2018-06-28 18:30:20 -0400

    Initial Commit

---
+++ UbiRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UbiRemote
+//
+//  Created by Kelly Smith on 6/28/18.
+//  Copyright © 2018 Kelly Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
